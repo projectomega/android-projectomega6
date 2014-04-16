19edf38ba97ae7cdc2fc022b8cfca7ca68e72055
gitmyhub <ajay.skootergofast@gmail.com>
2014-04-15 20:36:20 -0400

    Changes to Incorporate universal Screen Sizes

--- andr-projectomega6/src/com/example/andr_projectomega6/SearchActivity.java
+++ andr-projectomega6/src/com/example/andr_projectomega6/SearchActivity.java
@@ -6,15 +6,21 @@ import com.example.andr_projectomega6.QuoteDetail;
 import android.app.Activity;
 import android.content.Context;
 import android.content.Intent;
+import android.content.res.Resources;
 import android.os.Bundle;
+import android.util.DisplayMetrics;
 import android.util.Log;
+import android.view.Display;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.AdapterView;
 import android.widget.BaseAdapter;
 import android.widget.Button;
+import android.widget.FrameLayout;
 import android.widget.ImageView;
+import android.widget.LinearLayout;
+import android.widget.LinearLayout.LayoutParams;
 import android.widget.ListView;
 import android.widget.TextView;
 
@@ -23,10 +29,14 @@ import com.parse.ParseAnalytics;
 
 public class SearchActivity extends Activity{
 	private ListView mListView;
+	private Button UserButton;
+	private Button SearchButton;
+	private Button AddButton;
 	public class QuoteAdapter extends BaseAdapter {
 		private Context mContext;
 		private LayoutInflater mInflator;
 		private DataSource mDataSource;
+		
 		
 		public QuoteAdapter(Context c) {
 	        mContext = c;
@@ -70,6 +80,36 @@ public class SearchActivity extends Activity{
         super.onCreate(savedInstanceState);
         // setting default screen to login.xml
         setContentView(R.layout.activity_search);
+        UserButton = (Button) findViewById (R.id.user);
+        SearchButton = (Button) findViewById (R.id.search);
+        AddButton = (Button) findViewById (R.id.add);
+        
+        int someDimension = 50; //50pixels
+        UserButton.setWidth(someDimension);
+        UserButton.setHeight(someDimension);
+        
+/*        Display display = getWindowManager().getDefaultDisplay();
+        DisplayMetrics outMetrics = new DisplayMetrics ();
+        display.getMetrics(outMetrics);
+
+        float density  = getResources().getDisplayMetrics().density;
+        float dpHeight = outMetrics.heightPixels / density;
+        float dpWidth  = outMetrics.widthPixels / density;
+        dpWidth = ( dpWidth - 20 ) / 2;
+        
+        Resources resources = this.getResources();
+        DisplayMetrics metrics = resources.getDisplayMetrics();
+        float px = dpWidth * (metrics.densityDpi / 160f);*/
+        
+      //  LinearLayout.LayoutParams blp = new LinearLayout.LayoutParams(0, 0); 
+/*        LinearLayout.LayoutParams params = (LayoutParams) SearchButton.getLayoutParams();
+        int leftMargin = (int) (dpWidth * (metrics.densityDpi / 160f)); 
+        int width = (int) (25 * (metrics.densityDpi / 160f));
+        int height = (int) (25 * (metrics.densityDpi / 160f));
+        int topMargin = (int) (25 * (metrics.densityDpi / 160f));*/
+     //   params.setMargins(leftMargin, topMargin, 0, 0);
+ //       SearchButton.setLayoutParams(params); 
+        
 		mListView = (ListView) findViewById(R.id.quotes_list);
 		if (mListView == null) { Log.w("", "ListView is null"); }
 		mListView.setAdapter(new QuoteAdapter(this));
